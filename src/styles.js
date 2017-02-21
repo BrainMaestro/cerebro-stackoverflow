5f8ef54937738c70650397c54912a81965061402
Ezinwa Okpoechi <brainmaestro@outlook.com>
2017-02-21 16:22:49 +0100

    Remove unnecessary file

--- src/styles.js
+++
@@ -1,6 +0,0 @@
-// const bulma = require('bulma/css/bulma.css');
-const bulma = require('./styles.sass');
-
-module.exports = (...classes) => {
-  return classes.map(class_ => bulma[class_]).join(' ');
-};
